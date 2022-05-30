9633b7df0538e122e7bb602c6e9395c17ffbb220
atikxb <atikxb@gmail.com>
2022-05-30 22:14:14 +0600

    make admin feature added

--- src/Pages/Admin/MakeAdmin.js
+++ src/Pages/Admin/MakeAdmin.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
+import Loading from '../Loading/Loading';
 
 const MakeAdmin = () => {
+    const { isLoading, refetch, data: users } = useQuery('users', () =>
+        fetch(`http://localhost:5000/users`, {
+            method: 'GET',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json()
+            )
+    );
+    const makeAdmin = email => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 403) {
+                    console.log('You are not admin');
+                }
+                return res.json()
+            })
+            .then(data => {
+                if (data.modifiedCount) {
+                    toast.success("Successfully updated");
+                    refetch();
+
+                }
+            });
+
+    }
+    if (isLoading) { return <Loading /> }
     return (
-        <div>
-            MakeAdmin
+        <div className='container py-5'>
+            <h1 className="text-center">All Users</h1>
+            <hr style={{ width: '100px', margin: '-5px auto 40px auto', height: '2px', backgroundColor: 'black' }} />
+            <div class="row">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => user?.role !== 'admin' && <tr key={user._id}><td>{index + 1}</td><td>{user.displayName}</td><td>{user.email}</td><td><button onClick={() => makeAdmin(user.email)} className='btn btn-sm btn-success'>Make Admin</button></td></tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/SideNav/SideNav.js
+++ src/Pages/Shared/SideNav/SideNav.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import useAdmin from '../../../hooks/useAdmin';
+import Loading from '../../Loading/Loading';
 
 const SideNav = () => {
+    const [user, loading] = useAuthState(auth);
+    const [admin, adminLoading] = useAdmin(user);
+    if (loading || adminLoading) { return <Loading></Loading> }
     return (
         <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
             <div className="offcanvas-header">
@@ -11,12 +18,17 @@ const SideNav = () => {
             <div className="offcanvas-body">
                 <ul>
                     <li className="nav-item"><Link className="nav-link" to="/dashboard">My Profile</Link> </li>
-                    <li className="nav-item"><Link className="nav-link" to="/dashboard/add-review">Add a Review</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="myorders">My Orders</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/dashboard/manage-all-products">Manage all Orders</Link></li>
+                    {
+                        admin ? <>
+                        <li className="nav-item"><Link className="nav-link" to="/dashboard/manage-all-products">Manage all Orders</Link></li>
                     <li className="nav-item"><Link className="nav-link" to="/dashboard/add-product">Add a Product</Link></li>
                     <li className="nav-item"><Link className="nav-link" to="/dashboard/make-admin">Make Admin</Link></li>
                     <li className="nav-item"><Link className="nav-link" to="/dashboard/manage-products">Manage Products</Link></li>
+                    </> : <>
+                    <li className="nav-item"><Link className="nav-link" to="/dashboard/add-review">Add a Review</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="myorders">My Orders</Link></li>
+                        </>
+                    }
                 </ul>
             </div>
         </div>
